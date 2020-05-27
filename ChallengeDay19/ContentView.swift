2f0286623c3d8d9b5e14d2c86bdea0e4f747b1ca
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-05-27 09:53:31 +0900

    Challenge day
    https://www.hackingwithswift.com/100/swiftui/19

--- ChallengeDay19/ContentView.swift
+++ ChallengeDay19/ContentView.swift
@@ -9,13 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State private var inputWeight = ""
+    @State private var selection = 0
+    private let unit = ["kg", "metric ton"]
+    private let unitMagnification = [1_000, 1_000_000]
+    
+    var calculationWeight: Double {
+        let weight = Double(inputWeight) ?? 0
+        let magnification = Double(unitMagnification[selection])
+        let result = weight / magnification
+        
+        return result
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Section(header: Text("変換したい重量を入力してください")) {
+                    TextField("グラム", text: $inputWeight)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("変換したい単位を選択してください")) {
+                    Picker(selection: $selection, label: Text("単位")) {
+                        ForEach(0 ..< unit.count) {
+                            Text(self.unit[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("変換後の値")) {
+                    Text("\(calculationWeight) \(unit[selection])")
+                }
+            }
+            .navigationBarTitle("重量変換")
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
